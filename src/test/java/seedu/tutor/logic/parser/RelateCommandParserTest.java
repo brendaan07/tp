570c942333d166ac56b9944e0f3f124a72800630
Quek Syn Hui <56148373+synh@users.noreply.github.com>
2026-03-26 21:36:22 +0800

    Improve RelateCommandParserTest testcases

--- src/test/java/seedu/tutor/logic/parser/RelateCommandParserTest.java
+++ src/test/java/seedu/tutor/logic/parser/RelateCommandParserTest.java
@@ -12,12 +12,12 @@ public class RelateCommandParserTest {
     private final String[] validFormat = new String[] {" a\\test/test1/test/test", " a\\1/1a/1/1 a\\2/2a/2/2",
         "  d\\3/3a/3/3", " d\\4/4a/4/4 d\\5/5a/5/5 a\\6/6a/6/6",
         "    a\\ a  dqwd asd/ awd wd cawf / awefaw  awefsdfweg/sdfw wef    d\\sfwgrtb/  rg wrv/rgfr gree/erfe"};
-    private final String[] invalidFormat = new String[] {" a\\", " d\\", " a\\1", " d\\2/2", " a\\3/3/3",
-        " d\\5/5/5/5/5", " na\\test/test/test/test"};
-    private final String[] extraArgs = new String[] {" n/bruh a\\test/test/test/tes", " a\\1/1/1/1 e/ a\\2/2/2/2",
-        " d\\4/4/4/4 d\\5/5/5/5 p/0123456 a\\6/6/6/6"};
+    private final String[] invalidFormat = new String[] {" a\\", " d\\", " a\\1", " d\\2/2a", " a\\3/3a/3",
+        " d\\5/5a/5/5/5", " na\\test/test1/test/test"};
+    private final String[] extraArgs = new String[] {" n/bruh a\\test/test1/test/test", " a\\1/1a/1/1 e/ a\\2/2a/2/2",
+        " d\\4/4a/4/4 d\\5/5a/5/5 p/0123456 a\\6/6a/6/6"};
     private final String[] samePerson = new String[] {" a\\test/test/test/test", " a\\1/1/1/1 a\\2/2/2/2",
-        "  d\\3/3/3/3", " d\\4/4/4/4 d\\5/5/5/5 a\\6/6/6/6"};
+        "  d\\3/3/3/3 a\\1/2/3/4", " d\\4/4a/4/4 d\\5/5/5/5 a\\6/6/6/6"};
 
     @Test
     public void parse_validFormatInput_success() {
